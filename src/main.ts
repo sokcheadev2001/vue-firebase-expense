d41174cfc8c864209e773c685fc2173be199659c
sokcheadev2001 <sokchealeang.dev@gmail.com>
2024-03-14 21:42:15 +0700

    chore: :package: add vue-toastification

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import './assets/style.css'
+import 'vue-toastification/dist/index.css'
 import { createApp } from 'vue'
+import Toast from 'vue-toastification'
 import App from './App.vue'
 
 const app = createApp(App)
 
+app.use(Toast)
 app.mount('#app')
